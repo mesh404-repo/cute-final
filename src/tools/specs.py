d1ad641649f8c275417f1ba96807ab8bef31da76
star <star@gmail.com>
2026-01-26 07:55:22 +0300

    update

--- src/tools/specs.py
+++ src/tools/specs.py
@@ -450,8 +450,8 @@ TOOL_SPECS: dict[str, dict[str, Any]] = {
     "apply_patch": APPLY_PATCH_SPEC,
     "view_image": VIEW_IMAGE_SPEC,
     "update_plan": UPDATE_PLAN_SPEC,
-    # "web_search": WEB_SEARCH_SPEC,
-    # "fetch_url": FETCH_URL_SPEC,
+    "web_search": WEB_SEARCH_SPEC,
+    "fetch_url": FETCH_URL_SPEC,
     "transcript": TRANSCRIPT_SPEC,
 }
 
